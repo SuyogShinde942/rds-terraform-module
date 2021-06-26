e1fc07e92b1536ef0c143960bfd656bb1cd26f22
Suyog Shinde <64534620+SuyogShinde942@users.noreply.github.com>
2021-06-26 11:02:03 +0530

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,36 @@
+variable "allocated_storage"{
+
+}
+
+variable "storage_type"{
+
+}
+
+variable "engine"{
+
+}
+
+variable "engine_version"{
+
+}
+
+variable "instance_class"{
+
+}
+
+variable "name"{
+
+}
+
+variable "username"{
+
+}
+
+variable "password"{
+
+}
+
+
+variable "tags"{
+
+}
